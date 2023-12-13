4cbb66ae1ff86755e76af216cc95280771e3db6f
attiaimed <imed.attia@esprit.tn>
2023-12-13 14:38:17 +0100

    Yassine ye maalem

--- src/main/java/tn/esprit/sprint/foyer_attia_imed/Repositroy/BlockRepository.java
+++ src/main/java/tn/esprit/sprint/foyer_attia_imed/Repositroy/BlockRepository.java
@@ -2,10 +2,16 @@ package tn.esprit.sprint.foyer_attia_imed.Repositroy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.sprint.foyer_attia_imed.Entites.Bloc;
+import tn.esprit.sprint.foyer_attia_imed.Entites.Foyer;
+
+import java.util.List;
 
 public interface BlockRepository  extends JpaRepository<Bloc,Long> {
     Bloc findByNom(String nom);
     Bloc findByNomLike(String nom);
     Bloc findByNomContains(String nom);
     Bloc findByNomContaining(String nom);
+
+    List<Foyer> findByBlocsChambreTypec(String type);
+
 }
